b55ae897ab868ab4388129d6e2c1156a5129963f
kyrotomato <97704480+kyrotomato@users.noreply.github.com>
2022-07-22 21:13:03 -0500

    worked more on the imagespinner widget. I commented out text for now its gonna need to be its own widget or part of appbar. children created for list view just need to create container for elements and all that. follow psuedo code

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,10 @@ final Color mainVariant = Color.fromARGB(255, 0, 195, 255);
 
 //-----MAIN METHOD-----
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: SplashPage()));
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false, home: MainApp(),
+    //SplashPage()
+  ));
 }
 
 //------Pages-------
@@ -41,35 +44,47 @@ class SplashPage extends StatelessWidget {
   }
 }
 
-//Landing Page
+// landing page
 class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //items aligned downwards by scaffold
-      //background for page
-      backgroundColor: mainVariant,
-
-      // 2 buttons are child of main container but not other elements. on same level as text element
-      //create a card or delete cards buttons container
-      //create a card button
-      //delete card button
-    );
+        backgroundColor: mainVariant,
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ImageSpinner(),
+          ],
+        ));
   }
 }
-//-----Widgets------
-class ImageSpinner extends StatelessWidget
-@override
-Widget build(BuildContext context) {
-  return Container(
-      //child text element saying your cards 1
 
-      //child image carousel container 1
-      //child image carousel 2
+//-----Widgets------
+class ImageSpinner extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+        child: Container(
+      //added margin to move e cards down to fit better. Might need to move this later
+      margin: EdgeInsets.only(top: 40),
+      //child text element saying your cards 1
+      child:
+          // gonna add this to a seperate widget for simplicity and rendering across app
+          // child: Text(
+          //   'Your E-Cards',
+          //   style: TextStyle(fontSize: 30),
+          //),
+          //child image carousel container 1
+          ListView(
+        children: [
+          //child image carousel 2
+        ],
+      ),
+
       //child image elements 3
       //image elements contain a title 4
       //image elements contain a qr code 4
       //image elements contain a link 4
-  );
+    ));
+  }
 }
-
